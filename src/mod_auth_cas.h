04568ad18c5a338bbbf2da1637201107d1018b6d
Ray Davison <ray@sfu.ca>
2019-01-06 01:06:00 -0800

    Add routine definitions

--- src/mod_auth_cas.h
+++ src/mod_auth_cas.h
@@ -250,6 +250,17 @@ void cas_set_attributes(request_rec *r, cas_saml_attr *const attrs);
 const cas_saml_attr *cas_get_attributes(request_rec *r);
 int cas_match_attribute(const char *const attr_spec, const cas_saml_attr *const attributes, struct request_rec *r);
 
+/* Authorization check */
+#if MODULE_MAGIC_NUMBER_MAJOR < 20120211
+int cas_authorize(request_rec *r);
+int cas_authorize_worker(request_rec *r, const cas_saml_attr *const attrs, const require_line *const reqs, int nelts, const cas_cfg *const c);
+#else
+authz_status cas_check_authorization(request_rec *r, const char *require_line, const void *parsed_require_line);
+#endif
+
+/* Fancy wrapper around flock() */
+int cas_flock(apr_file_t *fileHandle, int lockOperation, request_rec *r);
+
 /* apr forward compatibility */
 #ifndef APR_FOPEN_READ
 #define APR_FOPEN_READ		APR_READ
